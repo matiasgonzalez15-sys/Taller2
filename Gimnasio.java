b24a364de16faf5444a0e7fa4ddbec9b3c60f53f
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-05-03 18:40:39 -0400

    Update Gimnasio.java

--- Gimnasio.java
+++ Gimnasio.java
@@ -1,5 +1,32 @@
-package taller2;
+package asd;
 
 public class Gimnasio {
+	private int numero;
+	private Persona lider;
+	private String derrotado;
+	
+	public Gimnasio(int numero, Persona lider, String derrotado) {
+		this.numero = numero;
+		this.lider = lider;
+		this.derrotado = derrotado;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public Persona getLider() {
+		return lider;
+	}
+
+	public String getDerrotado() {
+		return derrotado;
+	}
+
+	public void setDerrotado(String derrotado) {
+		this.derrotado = derrotado;
+	}
+	
+	
 
 }
