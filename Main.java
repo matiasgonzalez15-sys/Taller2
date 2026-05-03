4a03c0339128a949269fbe5f9c9e1a50f5cce979
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-05-03 18:40:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,205 +1,459 @@
 //Maximiliano Edhin Abd-El-Kader Gongora 22.128.215-9
 //Matías Ignacio González Gómez 22.350.340-3
-package taller2;
+package asd;
 
-import java.io.BufferedReader;
-
-import java.io.FileReader;
-
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-	static Scanner sc = new Scanner(System.in);
-	static LinkedList<Pokemon> listaPokemonsJugador = new LinkedList<Pokemon>();
-	static LinkedList<Habitat> listaHabitats = new LinkedList<Habitat>();
-	static LinkedList<Pokemon> listaPokemones = new LinkedList<Pokemon>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static Scanner sc = new Scanner(System.in);
+	public static List<Habitat> habitats = new LinkedList<Habitat>();
+	public static List<Gimnasio> gimnasios = new LinkedList<Gimnasio>();
+	public static List<Pokemon> pokemones = new LinkedList<Pokemon>();
+	public static int gimnasiosDerrotados = 0;
 
-		cargarHabitats("Habitats.txt", listaHabitats);
-		cargarPokemons("Pokedex.txt", listaPokemones);
+	public static void main(String[] args) throws FileNotFoundException {
+		leerHabitats();
+		leerPokemons();
+		leerGimnasios();
+		menuPrincipal();
 
-		System.out.println("Bienvenido!\n");
-		String opcion;
-		do {
-			System.out.println("1) Continuar.");
-			System.out.println("2) Nueva Partida.");
-			System.out.println("3) Salir. ");
-			System.out.print("> ");
-			opcion = sc.nextLine();
-			switch (opcion) {
-			case "1":
-
-				break;
-
-			case "2":
-				menuNuevaPartida();
-				break;
-
-			default:
-				break;
-			}
-
-		} while (!opcion.equals("3"));
-
-		sc.close();
 	}
 
-	public static void cargarHabitats(String ruta, LinkedList<Habitat> lista) {
-
-		try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				if (!line.strip().isEmpty()) {
-					Habitat nuevoHabitat = new Habitat(line);
-
-					lista.add(nuevoHabitat);
+	private static void leerGimnasios() throws FileNotFoundException {
+		File arch = new File("Gimnasios.txt");
+		Scanner lector = new Scanner(arch);
+		while (lector.hasNext()) {
+			String linea = lector.nextLine();
+			String[] partes = linea.split(";");
+			int numeroGim = Integer.valueOf(partes[0]);
+			String nombreGim = partes[1];
+			String estado = partes[2];
+			int cantPokemones = Integer.valueOf(partes[3]);
+			Persona p = new Persona(nombreGim);
+			for (int i = 4; i < cantPokemones + 4; i++) {
+				String nombre = partes[i];
+				Pokemon pok = buscarPokemon(nombre);
+				if (pok != null) {
+					p.agregarPokemon(pok);
 				}
-
 			}
-			br.close();
-		} catch (Exception e) {
-			// TODO: handle exception
+			Gimnasio g = new Gimnasio(numeroGim, p, estado);
+			gimnasios.add(g);
+
 		}
 
 	}
 
-	public static void cargarPokemons(String ruta, LinkedList<Pokemon> lista) {
-		try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				if (!line.strip().isEmpty()) {
-					String[] parts = line.split(";");
-					String nombre = parts[0];
-					String habitat = parts[1];
-					double porcentaje = Double.parseDouble(parts[2]);
-					int vida = Integer.parseInt(parts[3]);
-					int ataque = Integer.parseInt(parts[4]);
-					int defensa = Integer.parseInt(parts[5]);
-					int atqEsp = Integer.parseInt(parts[6]);
-					int defEsp = Integer.parseInt(parts[7]);
-					int velocidad = Integer.parseInt(parts[8]);
-					String tipo = parts[9];
-
-					if (parts.length == 10) {
-						Pokemon nuevoPokemon = new Pokemon(nombre, habitat, porcentaje, vida, ataque, defensa, atqEsp,
-								defEsp, velocidad, tipo);
-						lista.add(nuevoPokemon);
-					}
-
-				}
-
+	private static Pokemon buscarPokemon(String nombre) {
+		for (Pokemon p : pokemones) {
+			if (p.getNombre().equals(nombre)) {
+				return p;
 			}
-			br.close();
-		} catch (Exception e) {
-			// TODO: handle exception
+
+		}
+		return null;
+	}
+
+	private static void menuPrincipal() {
+		int opcion = -1;
+		while (opcion != 3) {
+			try {
+				System.out.println("1) Continuar.");
+				System.out.println("2) Nueva Partida.");
+				System.out.println("3) Salir");
+				System.out.print("Ingrese opcion: ");
+				opcion = Integer.valueOf(sc.nextLine());
+			} catch (Exception InputMismatchException) {
+				System.out.println("Ingrese una opcion valida");
+			}
+
+			switch (opcion) {
+			case 1:
+				break;
+			case 2:
+				crearPartidad();
+				break;
+			case 3:
+				System.out.println("Saliendo...");
+				break;
+			}
 		}
 
 	}
 
-	public static void menuNuevaPartida() {
+	private static void crearPartidad() {
+		System.out.println();
+		System.out.print("Ingrese apodo: ");
+		String apodo = sc.nextLine();
+		Persona p = new Persona(apodo);
+		System.out.println();
+		System.out.println("Bienvenido " + p.getNombre() + "!!!");
+		System.out.println();
+		int opcion = -1;
+		while (opcion != 8) {
+			try {
+				System.out.println(p.getNombre() + " , que deseas hacer?");
+				System.out.println();
+				System.out.println("1) Revisar equipo.");
+				System.out.println("2) Salir a capturar.");
+				System.out.println("3) Acceso al PC (cambiar Pokémon del equipo).");
+				System.out.println("4) Retar un gimnasio.");
+				System.out.println("5) Desafío al Alto Mando.");
+				System.out.println("6) Curar Pokémon.");
+				System.out.println("7) Guardar.");
+				System.out.println("8) Guardar y Salir.");
+				System.out.print("Ingrese opcion: ");
+				opcion = Integer.valueOf(sc.nextLine());
 
-		System.out.print("\nIngrese su apodo de jugador: ");
-		String nombreCuenta = sc.nextLine();
-		Persona jugador = new Persona(nombreCuenta, listaPokemonsJugador);
-
-		String opcion;
-		do {
-			System.out.println("\n" + jugador.getNombre() + ", que deseas hacer?");
-			System.out.println("\n1) Revisar equipo.");
-			System.out.println("2) Salir a capturar.");
-			System.out.println("3) Acceso al PC (cambiar Pokémon del equipo).");
-			System.out.println("4) Retar un gimnasio.");
-			System.out.println("5) Desafío al Alto Mando.");
-			System.out.println("6) Curar Pokémon.");
-			System.out.println("7) Guardar.");
-			System.out.println("8) Guardar y Salir.");
-			System.out.print("> ");
-			opcion = sc.nextLine();
-			switch (opcion) {
-			case "1":
-				System.out.println(listaPokemonsJugador.toString());
-				break;
-			case "2":
-				System.out.println("\nDonde deseas ir a explorar?\n");
-				System.out.println("Zonas disponibles:\n");
-				int i = 1;
-				for (Habitat habitat : listaHabitats) {
-					System.out.println(i + ") " + habitat);
-					i++;
-					if (i == 7) {
-						System.out.println(i + ") Volver al menu.");
-					}
-				}
-
-				System.out.print("\nIngrese una zona: ");
-				int opcionZona = sc.nextInt();
-				if (opcionZona > listaHabitats.size()) {
-					break;
-				}
-				Habitat habitatSeleccionado = listaHabitats.get(opcionZona - 1);
-
-				ArrayList<Pokemon> listaTempo = new ArrayList<Pokemon>();
-				for (Pokemon p : listaPokemones) {
-
-					if (habitatSeleccionado.toString().equals(p.getHabitat())) {
-
-						listaTempo.add(p);
-
-					}
-
-				}
-				sc.nextLine();
-
-				Random random = new Random();
-				int indiceRandom = random.nextInt(listaTempo.size());
-				Pokemon pokemonElegido = listaTempo.get(indiceRandom);
-				System.out.println("\nOHH! ha aparecido un " + pokemonElegido.getNombre() + "!!");
-				System.out.println("\nQue desea hacer?\n");
-				System.out.println("1) Capturar.");
-				System.out.println("2) Huir.\n");
-				System.out.print("Ingrese Opcion: ");
-				int op = sc.nextInt();
-				switch (op) {
-				case 1:
-					System.out.println("\n" + pokemonElegido.getNombre() + " capturado con exito!!");
-					listaPokemonsJugador.add(pokemonElegido);
-					System.out.println("\n" + pokemonElegido.getNombre() + " ha sido agregado a tu equipo!!");
-					break;
-				case 2:
-
-					System.out.println("\n huyendo.....");
-
-					break;
-				default:
-					break;
-				}
-
-				break;
-			case "3":
-
-				break;
-			case "4":
-
-				break;
-			case "5":
-
-				break;
-			case "6":
-
-				break;
-			case "7":
-
-				break;
-			default:
-
-				break;
+			} catch (Exception e) {
+				System.out.println("Ingrese una opcion valida");
+				System.out.println();
 			}
-		} while (!opcion.equals("8"));
+			switch (opcion) {
+			case 1:
+				verEquipo(p);
+				break;
+			case 2:
+				capturarPokemons(p);
+				break;
+			case 3:
+				accesoPC(p);
+				break;
+			case 4:
+				retarGimnasio(p);
+				break;
+			case 5:
+				break;
+			case 6:
+				curarPokemon(p);
+				break;
+			case 7:
+				break;
+			case 8:
+				System.out.println("Hasta luego entrenador!!");
+				System.out.println();
+				break;
+
+			}
+
+		}
 
 	}
+
+	private static void accesoPC(Persona p) {
+		
+		
+	}
+
+	private static void curarPokemon(Persona p) {
+		System.out.println();
+		for(Pokemon pok: p.getPokemons()) {
+			if(!pok.isVivo()) {
+				pok.setVivo(true);
+				System.out.println(pok.getNombre() + " ha sido curado!!");
+				
+			}
+		}
+		System.out.println();
+		
+	}
+
+	private static void retarGimnasio(Persona p) {
+		int opcion = -1;
+		try {
+			System.out.println();
+			System.out.println("A cual lider deseas retar?");
+			System.out.println();
+			int c = 1;
+			for (Gimnasio g : gimnasios) {
+				System.out.println(c + ") " + g.getLider().getNombre() + " - Estado: " + g.getDerrotado());
+				c++;
+			}
+			System.out.println("9) Volver al Menu");
+			System.out.println("Ingrese opcion: ");
+			opcion = Integer.valueOf(sc.nextLine());
+		} catch (Exception e) {
+			System.out.println("Ingrese una opcion valida");
+		}
+		Gimnasio gim = gimnasios.get(opcion - 1);
+		if (opcion - 1 > gimnasiosDerrotados) {
+			System.out.println("Calmado entrenador!!! No puedes retar a " + gim.getLider().getNombre()
+					+ " sin haber derrotado a los lideres anteriores!!!");
+
+		} else {
+			if (p.getPokemons().size() == 0) {
+				System.out.println("Captura pokemones antes de batallar en un gimnasio!!!");
+			} else {
+				System.out.println();
+				System.out.println("Desafiando a " + gim.getLider().getNombre() + "!!");
+				System.out.println();
+
+				boolean gane = peleaPokemones(gim.getLider(), p);
+				if (gane) {
+					System.out.println("Gimnasio derrotado!!");
+					gimnasiosDerrotados += 1;
+					gim.setDerrotado("Derrotado");
+				} else {
+					System.out.println("Volviendo al menu...");
+					System.out.println();
+				}
+			}
+
+		}
+
+		System.out.println();
+	}
+
+	private static boolean peleaPokemones(Persona lider, Persona p) {
+
+		int disponiblesA = lider.getPokemons().size();
+		int disponiblesB = p.getPokemons().size();
+		boolean seguimos = true;
+
+		int a = 0;
+		int b = 0;
+		while (seguimos) {
+			Pokemon pokemonA = lider.getPokemons().get(a); // gimnasio
+			Pokemon pokemonB = p.getPokemons().get(b); // usuario
+			int opcion = -1;
+			int statsA = pokemonA.getStatsTotales();
+			int statsB = pokemonB.getStatsTotales();
+			try {
+				System.out.println();
+				System.out.println(lider.getNombre() + " ha sacado a " + pokemonA.getNombre() + "!");
+				System.out.println(p.getNombre() + " ha sacado a " + pokemonB.getNombre() + "!");
+				System.out.println();
+				System.out.println("Que deseas hacer?");
+				System.out.println("1) Atacar");
+				System.out.println("2) Cambiar de pokemon");
+				System.out.println("3) Rendirse");
+				System.out.print("Ingrese una opcion: ");
+				opcion = Integer.valueOf(sc.nextLine());
+			} catch (Exception e) {
+				System.out.println("Ingrese una opcion valida");
+			}
+			switch (opcion) {
+			case 1:
+				System.out.println();
+				System.out.println(pokemonB.getNombre() + " -> " + statsB);
+				System.out.println(pokemonA.getNombre() + " -> " + statsA);
+
+				double multiplicador = TablaTipos.getEfectividad(pokemonB.getTipo(), pokemonA.getTipo());
+				if (multiplicador > 1.0) {
+					statsB = statsB * 2;
+					System.out.println();
+					System.out.println(pokemonB.getNombre() + " si es efectivo contra " + pokemonA.getNombre() + "!");
+					System.out.println("Nuevo puntaje:");
+					System.out.println(pokemonB.getNombre() + " -> " + statsB);
+					System.out.println(pokemonA.getNombre() + " -> " + statsA);
+				} else if (multiplicador < 1.0) {
+					statsB = statsB / 2;
+					System.out.println();
+					System.out.println(pokemonB.getNombre() + " no es efectivo contra " + pokemonA.getNombre() + "!");
+					System.out.println("Nuevo puntaje:");
+					System.out.println(pokemonB.getNombre() + " -> " + statsB);
+					System.out.println(pokemonA.getNombre() + " -> " + statsA);
+				}
+				if (statsB > statsA) {
+					System.out.println();
+					System.out.println("Ha ganado " + pokemonB.getNombre() + "! " + pokemonA.getNombre()
+							+ " ha sido derrotado...");
+					disponiblesA -= 1;
+					a++;
+
+				} else if (statsA > statsB) {
+					System.out.println();
+					System.out.println("Ha ganado " + pokemonA.getNombre() + "! " + pokemonB.getNombre()
+							+ " ha sido derrotado...");
+					pokemonB.setVivo(false);
+					disponiblesB -= 1;
+					for (int i = 0; i < p.getPokemons().size(); i++) {
+						if (p.getPokemons().get(i).isVivo()) {
+							b = i;
+							break;
+						}
+					}
+
+				} else {
+					System.out.println("Empate");
+				}
+				System.out.println();
+				if (disponiblesA == 0) {
+					seguimos = false;
+					return true;
+				}
+				if (disponiblesB == 0) {
+					System.out.println("Te has quedado sin pokemones en tu equipo...");
+					seguimos = false;
+				}
+
+				break;
+			case 2:
+				int c = 0; // contador para mostrar el equipo
+				int v = 0; // contador de los vivos
+				System.out.println();
+				if (p.getPokemons().size() == 1) {
+					System.out.println("No puedes cambiar. Solo tienes 1 pokemon en tu equpo !");
+				} else {
+					int opPokemon = -1;
+					try {
+						System.out.println("Que pokemon deseas usar?:");
+						System.out.println();
+						for (int i = 0; i < p.getPokemons().size(); i++) {
+							if (p.getPokemons().get(i).isVivo() && v < 6) {
+								System.out.println("Pokemon N." + (i + 1) + ") " + p.getPokemons().get(i).getNombre());
+								c++;
+								
+							}
+							v++;
+						}
+						System.out.print("Ingrese una opcion: ");
+						opPokemon = Integer.valueOf(sc.nextLine());
+					} catch (Exception e) {
+						System.out.println("Ingrese una opcion valida");
+					}
+					b = opPokemon - 1;
+
+				}
+				System.out.println();
+				break;
+			case 3:
+				System.out.println("Huyendo...");
+				seguimos = false;
+				break;
+			}
+
+		}
+
+		return false;
+
+	}
+
+	private static void capturarPokemons(Persona p) {
+		int opcion = -1;
+		try {
+			System.out.println();
+			System.out.println("Donde deseas ir a explorar?");
+			System.out.println();
+			System.out.println("Zonas disponibles:");
+			System.out.println();
+			for (int i = 0; i < habitats.size(); i++) {
+				System.out.println(i + 1 + ") " + habitats.get(i).getNombre());
+			}
+			System.out.println("7) Volver al menu.");
+			System.out.println();
+			System.out.print("Ingrese zona: ");
+			opcion = Integer.valueOf(sc.nextLine());
+
+		} catch (Exception e) {
+			System.out.println("Ingrese una opcion valida");
+		}
+		System.out.println();
+		Habitat habitatElegido = habitats.get(opcion - 1);
+		Random random = new Random();
+		int indiceRandom = random.nextInt(habitatElegido.getPokemones().size());
+		Pokemon pokemonRandom = habitatElegido.getPokemones().get(indiceRandom);
+		System.out.println("Oh!! Ha aparecido un increible " + pokemonRandom.getNombre());
+		System.out.println();
+		System.out.println("Que deseas hacer?");
+		System.out.println();
+		System.out.println("1) Capturar");
+		System.out.println("2) Huir");
+		System.out.print("Ingrese opcion: ");
+		int opcionPokemon = Integer.valueOf(sc.nextLine());
+		switch (opcionPokemon) {
+		case 1:
+			p.agregarPokemon(pokemonRandom);
+			System.out.println();
+			System.out.println(pokemonRandom.getNombre() + " capturado con exito!!");
+			System.out.println(pokemonRandom.getNombre() + " ha sido agregado a tu equipo.");
+			System.out.println();
+			break;
+		case 2:
+			System.out.println("Huyendo...");
+			break;
+		}
+
+	}
+
+	private static void verEquipo(Persona p) {
+		List<Pokemon> equipo = p.getPokemons();
+		if (equipo.isEmpty()) {
+			System.out.println("No tienes pokemons en tu equipo");
+		} else {
+			
+			p.mostrarEquipo();
+
+		}
+
+	}
+
+	private static void leerPokemons() throws FileNotFoundException {
+		File arch = new File("Pokedex.txt");
+		Scanner lector = new Scanner(arch);
+		while (lector.hasNext()) {
+			Pokemon p = null;
+			String linea = lector.nextLine();
+			String[] partes = linea.split(";");
+			String pokemon = partes[0];
+			String habitat = partes[1];
+
+			Habitat h = buscarHabitat(habitat);
+
+			double porcAparicion = Double.valueOf(partes[2]);
+			int aparicion = (int) (porcAparicion * 100);
+			int vida = Integer.valueOf(partes[3]);
+			int ataque = Integer.valueOf(partes[4]);
+			int defensa = Integer.valueOf(partes[5]);
+			int ataqueEspecial = Integer.valueOf(partes[6]);
+			int defensaEspecial = Integer.valueOf(partes[7]);
+			int velocidad = Integer.valueOf(partes[8]);
+			String tipo = partes[9];
+
+			if (h != null) {
+				p = new Pokemon(pokemon, h, aparicion, vida, ataque, defensa, ataqueEspecial, defensaEspecial,
+						velocidad, tipo);
+
+				h.agregarPokemon(aparicion, p);
+			} else {
+				p = new Pokemon(pokemon, h, aparicion, vida, ataque, defensa, ataqueEspecial, defensaEspecial,
+						velocidad, tipo);
+
+			}
+			p.sumarEstadisticas();
+			pokemones.add(p);
+
+		}
+
+	}
+
+	private static Habitat buscarHabitat(String habitat) {
+		for (Habitat h : habitats) {
+			if (h.getNombre().equals(habitat)) {
+				return h;
+			}
+		}
+		return null;
+
+	}
+
+	private static void leerHabitats() throws FileNotFoundException {
+		File arch = new File("Habitats.txt");
+		Scanner lector = new Scanner(arch);
+
+		while (lector.hasNext()) {
+			String linea = lector.nextLine();
+			String habitat = linea;
+			Habitat h = new Habitat(habitat);
+			habitats.add(h);
+		}
+
+	}
+
 }
