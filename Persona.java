6a55de82a9d9cf86844f8fbbb7949e278ec93744
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-05-03 18:43:40 -0400

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,38 +1,55 @@
-package taller2;
+package asd;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Persona {
 	private String nombre;
-	private LinkedList<Pokemon> pokemons;
+	private List<Pokemon> pokemons;
 	
+	public Persona(String nombre) {
+		this.nombre = nombre;
+		this.pokemons = new LinkedList<Pokemon>();
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
 
-	
-
-	public Persona(String nombre, LinkedList<Pokemon> pokemons) {
-		super();
-		this.nombre = nombre;
-		this.pokemons = pokemons;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Persona [nombre=" + nombre + ", pokemons=" + pokemons + "]";
-	}
-
-
-
-	public LinkedList<Pokemon> getPokemons() {
+	public List<Pokemon> getPokemons() {
 		return pokemons;
 	}
 
+	public void agregarPokemon(Pokemon pokemonRandom) {
+		pokemons.add(pokemonRandom);
+		
+	}
 
+	public void mostrarEquipo() {
+		int c = 0;
+		System.out.println("Tu equipo:");
+		System.out.println();
+		for(Pokemon p : pokemons) {
+			if(c < 6) {
+				System.out.println(p.getNombre() + " | " + p.getTipo() + " | Stats totales: "+ p.getStatsTotales());
+				c++;
+			}	
+		}
+		System.out.println();
+		
+		
+	}
 
+	public void mostrarPokemonsLider() {
+		System.out.println();
+		for(Pokemon p : pokemons) {
+			p.sumarEstadisticas();
+			System.out.println(p.getNombre() + " | " + p.getTipo());
+		}
+		System.out.println();
+		
+	}
 	
 	
+
 }
