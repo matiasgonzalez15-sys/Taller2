4bba058d8cce8e16f7982d482452d428fb2191aa
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-05-08 17:26:43 -0400

    Update Habitat.java

--- src/asd/Habitat.java
+++ src/asd/Habitat.java
@@ -19,7 +19,13 @@ public class Habitat {
 	public List<Pokemon> getPokemones() {
 		return pokemones;
 	}
-
+	
+	/**
+	 * Agrega un Pokemon a la lista tantas veces como indica su porcentaje de aparicion.
+	 * Por ejemplo, un Pokemon con 20% se agrega 20 veces.
+	 * @param aparicion Numero de veces que se agrega el Pokemon.
+	 * @param p El Pokemon a agregar.
+	 */
 	public void agregarPokemon(int aparicion, Pokemon p) {
 		for(int i = 0; i < aparicion; i++) {
 			pokemones.add(p);
